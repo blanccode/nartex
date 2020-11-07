9de2dc09e78521d321adc939ff56445f39fb7ebb
Hamza <canosari1@gmail.com>
2020-11-07 11:07:21 +0100

    pol finished

--- app.js
+++ app.js
@@ -1,14 +1,56 @@
+//////color-mode//////
+
+const engBtn = document.querySelector('.eng-btn')
+const polBtn = document.querySelector('.pol-btn')
+const html = document.querySelector('html');
+
+
+let myTheme = localStorage.getItem('theme')
+console.log(myTheme)
+if (myTheme != null && myTheme == 'light.css') {
+    light()
+} else {
+    dark()
+}
+
+
+engBtn.addEventListener('click', dark)
+polBtn.addEventListener('click', light)
+
+
+function light() {
+    changeTheme('light.css', "images/bubble-left-light.svg", "images/bubble-right-light.svg", "images/kartons-light.svg")
+    html.classList.remove('eng');
+    html.classList.add('pol');
+}
+function dark() {
+    changeTheme("styles.css", "images/bubble-left.svg", "images/bubble-right.svg", "images/kartons.svg")
+    html.classList.remove('pol');
+    html.classList.add('eng');
+}
+function changeTheme(sheet, leftBubble, rightBubble, kartons) {
+    localStorage.setItem('theme', sheet)
+    document.getElementById('stylesheet').href = sheet
+    document.getElementById('bubble-left').src = leftBubble
+    document.getElementById('bubble-right').src = rightBubble
+    document.getElementById('karton').src = kartons
+}
+
 window.addEventListener("load", function () {
     const burger = document.querySelector('.burger');
+    const html = document.querySelector('html');
+    const nav = document.querySelector('.nav-links');
+    const modal = document.querySelector('.nav');
+    const links = document.querySelectorAll('.nav-links li');
+    const a = nav.querySelectorAll('a');
 
     burger.addEventListener('click', openCloseMenu);
 
 
     function openCloseMenu() {
-
-        const html = document.querySelector('html');
-
         html.classList.toggle('open');
+
+        modal.classList.toggle('pointer')
 
         // LINK ANIMATION
         links.forEach((link, index) => {
@@ -16,20 +58,16 @@ window.addEventListener("load", function () {
         })
     }
 
-    const nav = document.querySelector('.nav-links');
-    const modal = document.querySelector('.nav');
-    const links = document.querySelectorAll('.nav-links li');
-    const a = nav.querySelectorAll('a');
 
 
 
     function closeWithModal() {
-        const html = document.querySelector('html');
 
         window.addEventListener('click', (e) => {
             if (e.target === modal) {
-
                 html.classList.toggle('open')
+                modal.classList.toggle('pointer')
+
                 links.forEach((link, index) => {
 
                     linkAnimationCheck(link, index)
@@ -40,6 +78,7 @@ window.addEventListener("load", function () {
 
     }
     closeWithModal()
+
 
 
 
@@ -56,4 +95,21 @@ window.addEventListener("load", function () {
         link.addEventListener('click', openCloseMenu)
 
     });
+
+   
+    // let jq = document.querySelector('#jquery').src
+    // jq = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js'
+    // if (jq == 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js') {
+    //     console.log(jq)
+        $("a").on("click", function (e) {
+            if (this.hash !== '') {
+                e.preventDefault();
+
+                const hash = this.hash;
+                $('html,body').animate({
+                    scrollTop: $(hash).offset().top
+                }, 800);
+            }
+        });
+    
 });
